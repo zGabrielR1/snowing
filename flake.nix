e7af87b8d349165486b8cff882feae5c0994244e
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-06-05 03:02:33 -0300

    .

--- flake.nix
+++ flake.nix
@@ -28,10 +28,10 @@
     #};
     hyprlock.url = "github:hyprwm/hyprlock";
     hypridle.url = "github:hyprwm/hypridle";
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
+    #hyprland-plugins = {
+    #  url = "github:hyprwm/hyprland-plugins";
+    #  inputs.hyprland.follows = "hyprland";
+    #};
     hyprspace = {
       url = "github:KZDKM/Hyprspace";
       inputs.hyprland.follows = "hyprland";
