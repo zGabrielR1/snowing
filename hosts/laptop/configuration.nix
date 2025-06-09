ba0aa2230742441081f0e9071a0676cdd3679987
zGabrielR1 <77293588+zGabrielR1@users.noreply.github.com>
2025-06-09 16:33:57 -0300

    .

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -14,7 +14,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_testing
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
