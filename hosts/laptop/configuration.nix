26836ca19bdbf40fbb9c2ce374553d0b0d856bfa
zGabrielR1 <77293588+zGabrielR1@users.noreply.github.com>
2025-06-12 17:18:47 -0300

    .

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -36,9 +36,9 @@
     "docker0" 
 ];
 # Enable the Cinnamon Desktop Environment.
-  #services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   #services.desktopManager.plasma6.enable = true;
-  services.displayManager.gdm.enable = true;
+  #services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
   #services.displayManager.lightdm.enable = true;
   #services.desktopManager.cinnamon.enable = true;
