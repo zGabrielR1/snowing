d98ece978f6ce20f409df45f3e73c16a7062a9e5
zGabrielR1 <77293588+zGabrielR1@users.noreply.github.com>
2025-06-19 00:57:35 -0300

    .

--- modules/home/profiles/zrrg/lazuhyprDotfiles/.config/nvim/lua/config/highlights.lua
+++
@@ -1,3 +0,0 @@
--- nvim-treesitter-context
-vim.api.nvim_set_hl(0, "TreesitterContextSeparator", { fg = "#27A1B9" })
-vim.api.nvim_set_hl(0, "TreesitterContext", {})
--- modules/home/profiles/zrrg/lazuhyprDotfiles/.config/wezterm/helpers.lua
+++
@@ -1,9 +0,0 @@
-local module = {}
-
-module.insert_multiple = function(t1, t2)
-    for _,v in ipairs(t2) do
-        table.insert(t1, v)
-    end
-end
-
-return module
