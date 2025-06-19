dcd269c7edf14652e9737840138c1430eca2be48
zGabrielR1 <77293588+zGabrielR1@users.noreply.github.com>
2025-06-19 01:24:56 -0300

    .

--- modules/home/profiles/zrrg/home.nix
+++ modules/home/profiles/zrrg/home.nix
@@ -1,196 +1,159 @@
 # modules/home/profiles/zrrg/home.nix
-# Base settings for user zrrg
+#
+# This is the primary Home Manager configuration for the user 'zrrg'.
+# It defines packages, aliases, dotfiles, and program settings.
+#
 { config, pkgs, system, inputs, ... }:
-{
-  home.username = "zrrg";
-  home.homeDirectory = "/home/zrrg";
 
-  # Packages that should be installed to the user profile.
-  home.packages = with pkgs; [
-    # Browser
-    inputs.zen-browser.packages."${system}".default 
-    # Windsurf IDE (latest version)
-    inputs.windsurf.packages."${system}".windsurf
-    
-    # File browsers and managers
-    pcmanfm
-    lf
-    ctpv
-    vesktop
-
-    # Terminal and shells
-    kitty
-    wezterm
-    tmux
-    fish
+let
+  # --- Package Sets for Better Organization ---
+  
+  shell-tools = with pkgs; [
+    # Shells & Prompts
     zsh
+    fish
     oh-my-zsh
     oh-my-posh
 
-    # Editors
-    helix
-    kakoune
-    micro
-    your-editor
-    vis
-    emacs
-    amp
-    ad
-    lapce
-    lite-xl
-    kdePackages.kate
-    zed-editor
-    neovim
-    # Development tools
-    lazygit
-    glab # gitlab
-    delta # git diff viewer
-    fzf
-    sesh # tmux session manager
-
-    # System utilities
-    optipng
-    jpegoptim
-    pfetch
-    btop
-    fastfetch
-    zip
-    xz
-    unzip
-    p7zip
-    brightnessctl
-    pavucontrol
-    networkmanagerapplet
-    polkit_gnome
-    refine
-    #gnome-tweaks
-
-    #hyprstellar
-    ctpv
-    lf
-    kitty
-    mpv
-    rofi-wayland
-    swaynotificationcenter
-    wlogout
-    wezterm
+    # Terminal Multiplexers
     tmux
-    hypridle
-    hyprlock
-    hyprshade
-    # Example: custom flake input package
-    # inputs.zen-browser.packages."${system}".specific
-    #hyprpaper
-    swww
-    udiskie
-    glab #gitlab
-    hyprsunset
-    swww
-    waybar
-    swaynotificationcenter
-    wlogout
-    rofi-wayland
-    wofi
-    wl-clipboard
-    slurp
-    grim
-    swappy
-    
-    pywal16
-
-    # Other utilities
-    udiskie
-    zoxide
-    dunst
-    mpv
-    spicetify-cli
-
-    # Fonts
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.symbols-only
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-
-    # Tmux plugins (for tpm)
     tmuxPlugins.sensible
     tmuxPlugins.vim-tmux-navigator
 
-    # Shell enhancements
-    zsh-syntax-highlighting
-    zsh-autosuggestions
-    zsh-completions
-
-    # Additional tools that might be needed
-    ripgrep
-    fd
+    # Core Utilities
+    zoxide
+    fzf
     bat
     eza
     lsd
     tree
+    ripgrep
+    fd
+    sd
+    unzip
+    p7zip
+    zip
+    xz
+
+    # System Monitors
+    btop
     htop
     bottom
+    fastfetch
+    pfetch
     du-dust
     procs
-    sd
-    tokei
-    hyperfine
-    bandwhich
+    
+    # Network Tools
     gping
     dog
-    # xh
-    # curlie
-    # as-tree
-    # choose
-    # jq
-    # yq
-    # htmlq
-    # pup
-    # csvkit
-    # miller
-    # q
-    # rush
-    # tuc
+    bandwhich
   ];
 
-  programs.zen-browser = {
-    enable = true;
-  };
+  dev-tools = with pkgs; [
+    # Git & Related
+    git
+    lazygit
+    gh
+    glab
+    delta
 
-  # Shell aliases
-  home.shellAliases = {
-    ll = "ls -l";
-    update = "nh os switch --flake ~/Snow/snowing";
-    # Additional aliases from dotfiles
-    ls = "lsd";
-    cat = "bat";
-    grep = "rg";
-    find = "fd";
-    top = "btm";
-    ps = "procs";
-    du = "dust";
-    df = "duf";
-    ping = "gping";
-    dig = "dog";
-    curl = "xh";
-    wget = "xh";
-    http = "xh";
-    https = "xh";
-  };
-  /*
-  # GTK Theming
-  gtk = {
-    enable = true;
-    font = {
-      name = "Noto Sans";
-      size = 11;
-    };
-    # theme.name = "Adwaita-dark";
-    # iconTheme.name = "Papirus-Dark";
-    # cursorTheme.name = "McMojave-cursors";
-  };
-  */
-  # Git configuration
+    # Editors & IDEs
+    neovim
+    helix
+    kakoune
+    micro
+    emacs
+    lapce
+    lite-xl
+    zed-editor
+    jetbrains-toolbox
+    (inputs.windsurf.packages."${system}".windsurf)
+
+    # Programming Language Support
+    tokei # code stats
+  ];
+
+  gui-apps = with pkgs; [
+    # Browser
+    (inputs.zen-browser.packages."${system}".default)
+
+    # File Managers
+    pcmanfm
+    nautilus
+    
+    # Utilities
+    bottles
+    refine
+    kdePackages.kate # For 'your-editor', it seems you want kate
+    vesktop
+  ];
+
+  hyprland-ecosystem = with pkgs; [
+    # Compositor & Core
+    hyprland
+    hyprpaper
+    hyprlock
+    hypridle
+    hyprshade
+    hyprsunset
+    
+    # Bar & Launchers
+    waybar
+    rofi-wayland
+    wofi
+
+    # System Utilities
+    swww
+    swaynotificationcenter
+    wlogout
+    wl-clipboard
+    slurp
+    grim
+    swappy
+    brightnessctl
+    pavucontrol
+    networkmanagerapplet
+    polkit_gnome
+    udiskie
+
+    # Theming
+    pywal16
+    spicetify-cli
+  ];
+  
+  fonts = with pkgs; [
+    # Nerd Fonts
+    (nerd-fonts.override { fonts = [ "JetBrainsMono" "FiraCode" "Meslo" ]; })
+    nerd-fonts.symbols-only
+    
+    # Standard Fonts
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    openmoji-color
+    twemoji-color-font
+  ];
+
+in
+{
+  home.username = "zrrg";
+  home.homeDirectory = "/home/zrrg";
+
+  # --- Combine all package lists ---
+  home.packages = 
+    shell-tools ++ 
+    dev-tools ++
+    gui-apps ++
+    hyprland-ecosystem ++
+    fonts;
+
+  # --- State Version ---
+  home.stateVersion = "25.11"; # Set to your current version
+
+  # --- Program Configurations ---
+  programs.home-manager.enable = true;
+
   programs.git = {
     enable = true;
     userName = "zGabrielR1";
@@ -198,7 +161,10 @@
     extraConfig = {
       init.defaultBranch = "main";
       core.editor = "nvim";
-      delta = {
+    };
+    delta = {
+      enable = true;
+      options = {
         navigate = true;
         light = false;
         side-by-side = true;
@@ -206,12 +172,63 @@
     };
   };
 
-  # Home Manager manages itself
-  programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    history.size = 10000;
+    # Source our beautiful, modular zshrc from the dotfiles
+    initExtra = "source ${config.xdg.configHome}/zshrc/20-customization";
+  };
+  
+  programs.tmux = {
+    enable = true;
+    # Let the dotfile handle the full configuration
+    extraConfig = "source-file ${config.home.homeDirectory}/.tmux.conf";
+  };
 
-  # Home Manager state version
-  home.stateVersion = "25.11";
+  # --- Dotfile Management ---
+  # This section links your dotfiles from the *single source* into the correct locations.
+  # Note the updated path to lazuhyprDotfiles.
+  xdg.configFile = {
+    "zshrc".source = ../../lazuhyprDotfiles/.config/zshrc;
+    "hypr".source = ../../lazuhyprDotfiles/.config/hypr;
+    "waybar".source = ../../lazuhyprDotfiles/.config/waybar;
+    "rofi".source = ../../lazuhyprDotfiles/.config/rofi;
+    "swaync".source = ../../lazuhyprDotfiles/.config/swaync;
+    "wlogout".source = ../../lazuhyprDotfiles/.config/wlogout;
+    "wezterm".source = ../../lazuhyprDotfiles/.config/wezterm;
+    "btop".source = ../../lazuhyprDotfiles/.config/btop;
+    "lf".source = ../../lazuhyprDotfiles/.config/lf;
+    "ctpv".source = ../../lazuhyprDotfiles/.config/ctpv;
+    "mpv".source = ../../lazuhyprDotfiles/.config/mpv;
+    "fastfetch".source = ../../lazuhyprDotfiles/.config/fastfetch;
+    "lsd".source = ../../lazuhyprDotfiles/.config/lsd;
+    "spicetify".source = ../../lazuhyprDotfiles/.config/spicetify;
+    "nvim".source = ../../lazuhyprDotfiles/.config/nvim;
+    "ohmyposh".source = ../../lazuhyprDotfiles/.config/ohmyposh;
+    "fish".source = ../../lazuhyprDotfiles/.config/fish;
+    "bashrc".source = ../../lazuhyprDotfiles/.config/bashrc;
+  };
+  
+  home.file = {
+    # Note the updated path to lazuhyprDotfiles.
+    ".zshrc".source = ../../lazuhyprDotfiles/.zshrc;
+    ".tmux.conf".source = ../../lazuhyprDotfiles/.tmux.conf;
+    ".vimrc".source = ../../lazuhyprDotfiles/.vimrc;
+    ".bashrc".source = ../../lazuhyprDotfiles/.bashrc;
+    ".gtkrc-2.0".source = ../../lazuhyprDotfiles/.gtkrc-2.0;
+    ".Xresources".source = ../../lazuhyprDotfiles/.Xresources;
+    ".ideavimrc".source = ../../lazuhyprDotfiles/.ideavimrc;
+    ".delta-themes.gitconfig".source = ../../lazuhyprDotfiles/.delta-themes.gitconfig;
+    ".stow-local-ignore".source = ../../lazuhyprDotfiles/.stow-local-ignore;
+  };
 
+  # Also import other modules for this user
   imports = [
+    ./wm/hyprland/default.nix
+    inputs.zen-browser.homeModules.beta
+    inputs.nix-index-db.hmModules.nix-index
   ];
 }
