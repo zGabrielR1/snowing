474c58d4616ee6ba5c1f338be66cefee441abe94
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-06-20 16:08:45 -0300

    .

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -67,7 +67,7 @@
   # Enable sound with pipewire.
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.zrrg = {
