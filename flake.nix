6ef271653219c0922ea99077cda1cb6c6107a91d
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-08-07 20:13:18 -0300

    .

--- flake.nix
+++ flake.nix
@@ -100,10 +100,9 @@
   };
 
   outputs = { self, nixpkgs, home-manager, flake-parts, chaotic, ... }@inputs:
-    flake-parts.lib.mkFlake {
-      inherit inputs; 
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-    } {
+    }
       perSystem = { config, pkgs, system, lib, ... }: {
         # Custom packages
         packages = {
