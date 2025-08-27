420eb9d1e8854efcdc6e4ef8c0cc962d590c95e0
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-08-27 00:38:18 -0300

    .

--- modules/home/flatpak-apps.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, ... }:
-{
-  # Enable pre-installation of Flatpak apps by default for the user
-  #services.flatpak-apps.refine.enable = lib.mkDefault true;
-  services.flatpak-apps.flatseal.enable = lib.mkDefault true;
-  services.flatpak-apps.kontainer.enable = lib.mkDefault true;
-} 
